9d8bfe1a9b12a0492d5dc16830e637d9dad4d281
François Ferland <git@francoisferland.com>
2015-11-19 17:08:15 +0100

    Adding a run_docker that automatically starts base nodes and set hostname correctly on client side.

---
+++ run_docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+HOST_IP=$(ip route | awk '/docker0/ { print $NF }')
+HOSTNAME=$(hostname)
+
+docker run --add-host=$HOSTNAME:$HOST_IP -t hbba_docker roslaunch hbba_synth base_nodes_r2.launch
