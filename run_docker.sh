353fbdf0a32911cd5310dd042c9e860389f3738a
François Ferland <git@francoisferland.com>
2015-11-20 10:59:49 +0100

    Removing pseudo-terminal option from run_docker script, now correctly stops the nodes and container when SIGTERM is sent.

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@
 HOST_IP=$(ip route | awk '/docker0/ { print $NF }')
 HOSTNAME=$(hostname)
 
-docker run --add-host=$HOSTNAME:$HOST_IP -t hbba_docker roslaunch hbba_synth base_nodes_r2.launch
+docker run --add-host=$HOSTNAME:$HOST_IP hbba_docker roslaunch hbba_synth base_nodes_r2.launch
