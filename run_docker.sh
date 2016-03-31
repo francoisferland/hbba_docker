346096ba35f951a30545608d28421321b182e4ae
François Ferland <git@francoisferland.com>
2016-03-31 12:10:16 +0200

    Adding a test to suppress 'empty ROS_HOSTNAME' warnings when running docker nodes.

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Now relying on --net host instead:
+# Now relying on --net host instead, uncomment this to do auto discovery of private IP:
 #HOST_IP=$(ip route | awk '/docker0/ { print $NF }')
 #HOSTNAME=$(hostname)
 
@@ -9,4 +9,9 @@ if [[ "$(docker images -q francoisferland/hbba 2> /dev/null)" == "" ]]; then
     exit 1
 fi
 
+# If ROS_HOSTNAME is unset, set it default (hostname). Simply avoids warning messages when starting nodes.
+if [[ $ROS_HOSTNAME == "" ]]; then
+    ROS_HOSTNAME=$(hostname)
+fi
+    
 docker run --net host -e ROS_HOSTNAME=$ROS_HOSTNAME -e ROS_MASTER_URI=$ROS_MASTER_URI francoisferland/hbba roslaunch hbba_synth base_nodes_r2.launch
